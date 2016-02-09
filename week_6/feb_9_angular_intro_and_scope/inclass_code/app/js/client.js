59ff1488c09fb037e3b6d8607cfc5873b68202a5
toastynerd <tylermorgan86@gmail.com>
2016-02-09 15:09:47 -0800

    end of scope stuff

--- week_6/feb_9_angular_intro_and_scope/inclass_code/app/js/client.js
+++ week_6/feb_9_angular_intro_and_scope/inclass_code/app/js/client.js
@@ -1,1 +1,11 @@
 const angular = require('angular');
+
+const bearsApp = angular.module('bearsApp', []);
+
+bearsApp.controller('bearsController', ['$scope', ($scope) => {
+  $scope.greeting = 'hello world';
+}]);
+
+bearsApp.controller('anotherController', ['$scope', ($scope) => {
+  $scope.greeting = '';
+}]);
