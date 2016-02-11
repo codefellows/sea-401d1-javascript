bd659d7de5a5fa36d0c01e5fc9c2b71990517666
toastynerd <tylermorgan86@gmail.com>
2016-02-11 15:21:27 -0800

    start of angular testing

---
+++ week_6/feb_11_angular_testing/inclass_code/gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const webpack = require('webpack-stream');
+
+gulp.task('html:dev', () => {
+  gulp.src(__dirname + '/app/**/*.html')
+    .pipe(gulp.dest(__dirname + '/build'));
+});
+
+gulp.task('webpack:dev', () => {
+  gulp.src(__dirname + '/app/js/client.js')
+    .pipe(webpack({
+      output: {
+        filename: 'bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build:dev', ['webpack:dev', 'html:dev']);
+gulp.task('default', ['build:dev']);
