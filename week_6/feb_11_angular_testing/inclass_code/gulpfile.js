d52d583d54dd4c34a7035be326f718b6dfe571d1
toastynerd <tylermorgan86@gmail.com>
2016-02-11 16:08:34 -0800

    end of angular testing day one

--- week_6/feb_11_angular_testing/inclass_code/gulpfile.js
+++ week_6/feb_11_angular_testing/inclass_code/gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
+const babel = require('babel-loader');
 
 gulp.task('html:dev', () => {
   gulp.src(__dirname + '/app/**/*.html')
@@ -15,6 +16,17 @@ gulp.task('webpack:dev', () => {
     }))
     .pipe(gulp.dest('build/'));
 });
+
+gulp.task('webpack:test', () => {
+  gulp.src(__dirname + '/test/test_entry.js')
+    .pipe(webpack({
+      output: {
+        filename: 'test_bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('test/'));
+});
+
 
 gulp.task('build:dev', ['webpack:dev', 'html:dev']);
 gulp.task('default', ['build:dev']);
---
+++ week_6/feb_11_angular_testing/inclass_code/test/test_entry.js
@@ -0,0 +1,5 @@
+describe('it should do some stuff', () => {
+  it('should work after a build', () => {
+    expect(true).toBe(true);
+  });
+});
