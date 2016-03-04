b7ba1066c66fa941714bab009bb4ce2554f05490
toastynerd <tylermorgan86@gmail.com>
2016-03-03 16:03:31 -0800

    end of day

--- week_4/jan_26_authentication_2_electric_boogaloo/inclass_code/server.js
+++ week_4/jan_26_authentication_2_electric_boogaloo/inclass_code/server.js
@@ -5,16 +5,18 @@ mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/bears_app_dev'
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, token');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
 
 const bearsRouter = require(__dirname + '/routes/bears_routes');
 const authRouter = require(__dirname + '/routes/auth_routes');
+const userRouter = require(__dirname + '/routes/user_routes');
 
 app.use('/api', bearsRouter);
 app.use('/api', authRouter);
+app.use('/api', userRouter);
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('server up on port: ' + PORT));
